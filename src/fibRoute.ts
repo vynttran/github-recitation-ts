cc19dd422c1b9929e4dff73bb411e8afb873f65a
Vy <112414752+vynttran@users.noreply.github.com>
2024-01-22 01:16:15 +0700

    Fix issues related to ‘any’ value and unsafe access

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,11 +1,22 @@
 // Endpoint for querying the fibonacci numbers
 
+import { Request, Response } from 'express';
 import fibonacci from "./fib";
 
-export default (req, res) => {
-  const { num } = req.params;
+export default (req: Request, res: Response) => {
+  const numStr = req.params.num;
+  if (typeof numStr !== 'string') {
+    res.status(400).send('Invalid input');
+    return;
+  }
+  
+  const num = parseInt(numStr, 10);
+  if (isNaN(num)) {
+    res.status(400).send('Input must be a number');
+    return;
+  }
 
-  const fibN = fibonacci(parseInt(num));
+  const fibN = fibonacci(num);
   let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
